d324985dacf8f8e231edfc049a03e1391285f6fd
Maleesha dilukshan <133895520+matheeshadunal@users.noreply.github.com>
2024-10-08 13:36:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CqgBQQpZ#ECrYiPh468jHdiPp4N8d2lqQsUfSD3rOHgxUemG7vmI",
+MONGODB: process.env.MONGODB || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority",//enter mongo db url
 };
